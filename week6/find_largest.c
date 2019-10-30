ff1bc630b9405ec7d5b250ad336328cfe93c46b3
Halil Durmuş <durmushalil@gmail.com>
2019-10-30 14:19:17 +0300

    Array size changed to macro

--- week6/find_largest.c
+++ week6/find_largest.c
@@ -7,16 +7,18 @@
 //Random integer generator copied from stackoverflow
 //Search for code block online and use them in your code
 
+#define ARRAY_MAX 10
+
 int main()
 {
 
-    int array[10];
+    int array[ARRAY_MAX];
     int largest = 0;
     srand(time(NULL));   // Initialization, should only be called once.
 
 
     printf("First Generate Array of Random Integers\n");
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < ARRAY_MAX; i++)
     {
         array[i] = rand() % 100;      // Returns a pseudo-random integer between 0 and 100.
         printf("%d ", array[i]);
@@ -24,7 +26,7 @@ int main()
     printf("\n");
 
     //Check if the array element is larger than largest variable
-    for(int i=0; i<10; i++)
+    for(int i=0; i<ARRAY_MAX; i++)
     {
         if(array[i] > largest)
             largest = array[i];
